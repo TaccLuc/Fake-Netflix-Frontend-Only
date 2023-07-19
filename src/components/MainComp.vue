ac0b8d2c803e47b4abc9f54c441e73e74b5f96a5
TaccLuc <tacc.luca@gmail.com>
2023-07-19 17:17:42 +0200

    Add: images and stars rating

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -21,13 +21,27 @@ export default{
 
         <div class="row justify-content-center">
 
-          <SingleMovie 
-          v-for="(singleMovie, i) in store.movies" :key="i"
-          :singleMovie="singleMovie"/>
+          <div v-if="store.movies.length != 0">
 
-          <SingleMovie 
-          v-for="(singleMovie, i) in store.series" :key="i"
-          :singleMovie="singleMovie"/>
+            <SingleMovie 
+            v-for="(singleMovie, i) in store.movies" :key="i"
+            :singleMovie="singleMovie"
+            :title="singleMovie.title"
+            :ogTitle="singleMovie.original_title"
+            :poster="singleMovie.poster_path"/>
+
+          </div>
+
+          <div v-if="store.series.length != 0">
+
+            <SingleMovie
+            v-for="(singleMovie, i) in store.series" :key="i"
+            :singleMovie="singleMovie"
+            :title="singleMovie.name"
+            :ogTitle="singleMovie.original_name"
+            :poster="singleMovie.poster_path"/>
+
+          </div>
 
         </div>
 
--- src/components/MainComps/SingleMovie.vue
+++ src/components/MainComps/SingleMovie.vue
@@ -1,22 +1,34 @@
 <script>
-import { store } from '../../store'
+import { store } from '../../store';
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 
+
 export default{
-  data() {
+    data() {
     return {
         store,
         lang: true
     }
   },
   props: {
-    singleMovie: Object
-  },
+      singleMovie: Object,
+      title: String,
+      ogTitle: String,
+      poster: String
+    },
+    mounted() {
+      () => {
+          console.log(poster)
+      }
+    },
   computed: {
     language() {
         const lang = this.singleMovie.original_language;
         if ( lang == 'en') {
             return 'gb';
+        }
+        else if (lang == 'ja') {
+            return 'jp';
         }
         else if(lang == 'it' &&
         lang == 'fr' &&
@@ -40,6 +52,13 @@ export default{
         else {
             this.lang = false;
         }
+    },
+    vote() {
+        if (parseInt(this.singleMovie.vote_average) > 1 || !NaN) {
+            const halfVote = Math.round(parseInt(this.singleMovie.vote_average) / 2);
+        
+            return halfVote;
+        }
     }
   }
 }
@@ -49,27 +68,48 @@ export default{
 <template>
 
     <div class="mb-5">
-        <h3>
-             {{ singleMovie.title }}
-             {{ singleMovie.name }}
-        </h3>
+        
+        <div>
+            <img v-if="poster != null" :src="`https://image.tmdb.org/t/p/w342${poster}`" :alt="title">
+            <div v-else>
+                IMAGE NOT FOUND
+            </div>
+        </div>
+        
+        <div>
 
-        <h3>
-            {{ singleMovie.original_title }}
-            {{ singleMovie.original_name }}
-            
-        </h3>
+            <h3>
 
-        <h3>
-            <span v-if="lang" :class="`fi fi-${language}`"></span>
-            <span v-else>
-                {{ singleMovie.original_language }}
-            </span>
-        </h3>
+                {{ title }}
 
-        <h3>
-            {{ singleMovie.vote_average }}
-        </h3>
+            </h3>
+
+            <h3>
+
+                {{ ogTitle }}   
+
+            </h3>
+
+            <h3>
+
+                <span v-if="lang" :class="`fi fi-${language}`"></span>
+
+                <span v-else>
+
+                    {{ singleMovie.original_language }}
+
+                </span>
+
+            </h3>
+
+            <h3>
+
+                <i v-for="i in vote" :key="i" class="fa-solid fa-star"></i>
+                <i v-for="j in (5 - vote)" :key="j" class="fa-regular fa-star"></i>
+
+            </h3>
+
+        </div>
 
     </div>
 
